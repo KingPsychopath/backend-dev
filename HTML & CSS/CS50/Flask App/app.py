10c0bfa62b0b3058b757ae41a4437c074b54d141
KingPsychopath <dev@KingPsychopat.com>
2024-02-06 05:16:02 +0000

    Working Path/Route submit Flask Web-app, Client-side error/type checking (wrong)

--- HTML & CSS/CS50/Flask App/app.py
+++ HTML & CSS/CS50/Flask App/app.py
@@ -4,16 +4,22 @@ app = Flask(__name__) # create a new Flask instance
 
 @app.route("/")
 def index():
-    # requests the name from the URL query string (e.g. ?name=Owen)
-    name = request.args.get("name")
-    
+    # the thing on the right is the actual variable you want to pass to the template file from above
+    return render_template("index.html")
+
+@app.route("/greet")
+def greet():
     # if name is not provided, set it to "World"
-    if not name:
-        name = "World"
+
+    # requests the name from the URL query string (e.g. ?name=Owen)
+    name = request.args.get("name", "World")
+    
+    # if name is not provided, set it to "World" - not needed as there is a default value in the request.args.get() method
+    #if not name:
+    #    name = "World"
 
     # name=name - thing of the left is the variable you plan to use in the template file
-    # the thing on the right is the actual variable you want to pass to the template file from above
-    return render_template("index.html", name=name)
+    return render_template("greet.html", name=name)
 
 if __name__ == "__main__":
     app.run(debug=True) # run the Flask app in debug mode
